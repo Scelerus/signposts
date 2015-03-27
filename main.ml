9d16f88ddc753635c20b13bf56d7e19e92e094ce
Scelerus <scelerus@scelerus-1015E.(none)>
2015-03-27 13:49:34 -0400

    lsp now reads in stdin and produces an (internal) list of lines. it then outputs the number of lines read

--- main.ml
+++ main.ml
@@ -1,6 +1,13 @@
+let debug = ref true
 
 let main () =
-  Printf.printf "hello world"
+  let lines = ref [] in
+  try
+    while true do
+      lines := (read_line()) :: !lines
+    done
+  with _ -> begin if !debug then Printf.printf "lines read in: %d\n" (List.length !lines) end;
+    Printf.printf "done\n"
 ;;
 
 main ();;
